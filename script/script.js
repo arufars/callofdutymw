461bbe97d431ce038133c43fcf648330431de24e
Ghifar Rito <firselee@gmail.com>
2021-10-26 15:36:11 +0700

    codmw

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// video
+document.getElementById('btnPause').addEventListener('click', function(){
+    let video = document.getElementById('video')
+    video.pause();
+})
+document.getElementById('btnPlay').addEventListener('click', function(){
+    let video = document.getElementById('video')
+    video.play();
+})
+
