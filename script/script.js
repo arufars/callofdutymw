db088b2ac1115875381584a70d3d9eae5681d556
Ghifar Rito <firselee@gmail.com>
2021-10-27 23:17:40 +0700

    penambahan section

--- script/script.js
+++ script/script.js
@@ -1,10 +1,24 @@
 // video
-document.getElementById('btnPause').addEventListener('click', function(){
-    let video = document.getElementById('video')
-    video.pause();
-})
-document.getElementById('btnPlay').addEventListener('click', function(){
-    let video = document.getElementById('video')
-    video.play();
-})
+// document.getElementById('btnPause').addEventListener('click', function(){
+//     let video = document.getElementById('video')
+//     video.pause();
+// });
+// document.getElementById('btnPlay').addEventListener('click', function(){
+//     let video = document.getElementById('video')
+//     video.play();
+// });
 
+const selectElement = document.querySelector('.ice-cream');
+
+selectElement.addEventListener('change', (event) => {
+  const result = document.querySelector('.result');
+  result.textContent = `Country ${event.target.value}`;
+});
+
+
+const selectCod = document.querySelector('.cod');
+
+selectCod.addEventListener('change', (e) => {
+  const hasil = document.querySelector('.hasil');
+  hasil.textContent = `Title :  ${e.target.value}`;
+});
